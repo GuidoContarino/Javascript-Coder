457b8dbbdf4fcce3019d9f68e618233c65c03903
GuidoContarino <107894498+GuidoContarino@users.noreply.github.com>
2022-08-23 18:22:40 -0300

    Add files via upload

---
+++ Coder-JavaScript/js/main.js
@@ -0,0 +1,6 @@
+let nombredeusuario = prompt("ingresar nombre de usuario");
+if (nombredeusuario == ""){
+    alert("no ingresaste nombre de usuario")
+}else{
+    alert("nombre de usuario ingresado "+ nombredeusuario);
+}
