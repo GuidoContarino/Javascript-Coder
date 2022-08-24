83c26432510701ebc20153d1b984fca45ea76e89
Guido Contarino <guidoconta11@gmail.com>
2022-08-24 12:20:27 -0300

    Ciclos

--- Coder-JavaScript/js/main.js
+++
@@ -1,6 +0,0 @@
-let nombredeusuario = prompt("ingresar nombre de usuario");
-if (nombredeusuario == ""){
-    alert("no ingresaste nombre de usuario")
-}else{
-    alert("nombre de usuario ingresado "+ nombredeusuario);
-}
