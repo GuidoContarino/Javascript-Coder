d07e61b47b46ccdd24e26727c1f303eb34441629
Guido Contarino <guidoconta11@gmail.com>
2022-08-29 17:14:39 -0300

    Funciones

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,4 +1,4 @@
-//algoritmo con un condicional while y switch.
+/*algoritmo con un condicional while y switch.
 let entrada = prompt("Ingrese Su Nombre");
 //algoritmo que finaliza cuando se presione ESC.
 while (entrada != "esc") {
@@ -32,5 +32,23 @@ while (entrada != "esc") {
         alert(entrada + " Ya has finalizado su trabajo por hoy");
     }else{
         alert(entrada + " Usted no finalizo su trabajo");
+    }
+}*/
+
+function convertir(){
+    var valor = parseInt(document.getElementById("valor").value);
+    var Resultado = 0;
+    var Dolar = 138.31;
+    var Euro = 138.22;
+    if(document.getElementById("uno").Checked){
+        Resultado = valor * Dolar;
+        alert("El cambio de Pesos a Dolares es: $" + Resultado)
+    }
+    else if (document.getElementById("dos").Checked){
+        Resultado = valor * Euro;
+        alert("El cambio de Pesos a Euros es: $" + Resultado)
+    }
+    else{
+        alert("Tienes que completar todos los requerimientos")
     }
 }
